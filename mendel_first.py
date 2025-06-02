ed65cac9dc2c7d7fb7c9ac18f0cdba15ea636476
aclcheuk <41525634+aclcheuk@users.noreply.github.com>
2025-06-02 14:35:39 +0100

    Complete mendel_first.py

--- mendel_first.py
+++ mendel_first.py
@@ -30,19 +30,17 @@ def calc_offspring_probs(homo_dominant:int, hetero:int, homo_recessive:int)->flo
 
     # Offspring Genotype Probabilities
     prob_DD = (prob_k1_k2) + (0.5*prob_k1_m2) + (0.5*prob_m1_k2) + (0.25*prob_m1_m2)
-    prob_Dd = (0.25*prob_k1_m2) + (prob_k1_n2) + (0.25*prob_m1_k2) + (0.5*prob_m1_m2) + (prob_n1_k2) + (0.5*prob_n1_m2)
-    prob_dd = (0.25*prob_k1_m2) + (0.25*prob_m1_k2) + (0.25*prob_m1_m2) + (prob_m1_n2) +(0.5*prob_n1_m2) + (prob_n1_n2)
+    prob_Dd = (0.5*prob_k1_m2) + (prob_k1_n2) + (0.5*prob_m1_k2) + (0.5*prob_m1_m2) + (0.5*prob_m1_n2) + (prob_n1_k2) + (0.5*prob_n1_m2)
+    prob_dd = (0.25*prob_m1_m2) + (0.5*prob_m1_n2) +(0.5*prob_n1_m2) + (prob_n1_n2)
     offspring_probs = [prob_DD, prob_Dd, prob_dd]
     sum_offspring_probs = sum(offspring_probs)
     print("Sum of offspring probabilities: ", sum_offspring_probs)
-    return (1-prob_dd)
+    return (prob_DD+prob_Dd)
+#26 22 26
 
-k = 2
-m = 2
-n = 2
-
-a = calc_offspring_probs(k,m,n)
-print(a)
+k = 26
+m = 22
+n = 26
 
 prob_dom = calc_offspring_probs(k,m,n)
 print("Probability of Dominant offspring = ", prob_dom)
